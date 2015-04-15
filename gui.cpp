a6d761c2b282eb5195fe8a24d5a0ec780203f17c
evpobr <evpobr@gmail.com>
2015-04-15 15:27:00 +0500

    Enable theming. Closes #1.

--- gui.cpp
+++ gui.cpp
@@ -22,6 +22,18 @@
  */
 #define _WIN32_WINNT 0x0501 // Needed for WM_MOUSEWHEEL
 
+// #ifdef _UNICODE // disabled because works for non-unicode applications as well
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_IA64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+// #endif
+
 #include <windows.h>
 #include "resource.h"
 #include "Shlwapi.h"
