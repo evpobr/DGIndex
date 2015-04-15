cdb0a712a95108fafcc670aa1ee6422ac4672f05
evpobr <evpobr@gmail.com>
2015-04-15 15:32:43 +0500

    #2 Font changed to MS Shell Dlg for AVS template dialog.

--- gui.cpp
+++ gui.cpp
@@ -22,7 +22,7 @@
  */
 #define _WIN32_WINNT 0x0501 // Needed for WM_MOUSEWHEEL
 
-// #ifdef _UNICODE // disabled because works for non-unicode applications as well
+// #ifdef _UNICODE // disabled because works for non-unicode as well
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #elif defined _M_IA64
